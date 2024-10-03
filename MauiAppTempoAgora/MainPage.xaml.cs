4db66810302699533a512f292dd688beeb09cce6
DerekNuness <derekcartola2@gmail.com>
2024-10-02 22:06:44 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppTempoAgora/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿namespace MauiAppTempoAgora
+{
+    public partial class MainPage : ContentPage
+    {
+        CancellationTokenSource _cancelTokenSource;
+        bool _isCheckingLocation;
+
+        string? cidade;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+           try
+            {
+                _cancelTokenSource = new CancellationTokenSource();
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                Location? location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                {
+                    lbl_latitude.Text = location.Latitude.ToString();
+                    lbl_longitude.Text = location.Longitude.ToString();
+                }
+            }
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button_Clicked_2(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
